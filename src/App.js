ced47f57f535bb17db069669cb327ed952bf50d1
choi9970 <j.choi9970@gmail.com>
2025-07-16 23:03:37 +0900

    20250716 JSX 와 props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+function Food({fav}){
+  
+  return (<h1>I LOVE {fav}</h1>);
+}
 
 function App() {
+  //return <div className="App"/>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <h1>Hello</h1>
+        <Food fav="Kimchi" />
+        <Food fav="Kimchi1" />
+        <Food fav="Kimchi2" />
+
+      </div>
   );
 }
 
