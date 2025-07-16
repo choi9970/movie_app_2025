aea6953e293a83bff7aee0eac5c02948ec69b341
choi9970 <j.choi9970@gmail.com>
2025-07-17 07:15:21 +0900

    20250717 92page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-function Food({fav}){
+function Food({name,picture}){
   
-  return (<h1>I LOVE {fav}</h1>);
+  return (
+    <div>
+      <h1>I LOVE {name}</h1>
+      <img src={picture}/>
+    </div>
+    
+  );
+}
+
+const foodLike =[
+  {name:'kimchi',
+   image:'https://shop.hansalim.or.kr/shopping/is/itm/100101024/100101024_1_568.jpg'
+  }
+  ,{name:'ramen',
+   image:'https://m.health.chosun.com/site/data/img_dir/2022/09/23/2022092301908_0.jpg'}
+];
+
+function randerFood(dish){
+  return <Food name={dish.name} picture={dish.image}/>
 }
 
 function App() {
   //return <div className="App"/>;
   return (
     <div>
-        <h1>Hello</h1>
-        <Food fav="Kimchi" />
-        <Food fav="Kimchi1" />
-        <Food fav="Kimchi2" />
-
-      </div>
+        
+      {foodLike.map(randerFood)}
+    </div>
   );
 }
 
