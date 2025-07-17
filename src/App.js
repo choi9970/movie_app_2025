3c5a10fa58f66eb8c1fb22e532374e0b12bb21b6
choi9970 <j.choi9970@gmail.com>
2025-07-17 23:50:55 +0900

    props의 이해

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-function Food({name,picture}){
+import PropTypes, { string } from "prop-types";
+
+function Food({name,picture,rating}){
   
   return (
     <div>
       <h1>I LOVE {name}</h1>
-      <img src={picture}/>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name}/>
     </div>
     
   );
 }
 
 const foodLike =[
-  {name:'kimchi',
-   image:'https://shop.hansalim.or.kr/shopping/is/itm/100101024/100101024_1_568.jpg'
+  {
+    id:1,
+    name:'kimchi',
+   image:'https://shop.hansalim.or.kr/shopping/is/itm/100101024/100101024_1_568.jpg',
+   rating : 5
   }
-  ,{name:'ramen',
-   image:'https://m.health.chosun.com/site/data/img_dir/2022/09/23/2022092301908_0.jpg'}
+  ,{
+    id:2,
+    name:'ramen',
+   image:'https://m.health.chosun.com/site/data/img_dir/2022/09/23/2022092301908_0.jpg',
+   rating : 4.9}
 ];
-
-function randerFood(dish){
-  return <Food name={dish.name} picture={dish.image}/>
-}
 
 function App() {
   //return <div className="App"/>;
   return (
     <div>
         
-      {foodLike.map(randerFood)}
+      {foodLike.map(dish=> (<Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>))}
     </div>
   );
 }
+
+Food.propTypes ={
+  name:PropTypes.string.isRequired,
+  image:PropTypes.string.isRequired,
+  rating:PropTypes.number
+};
 
 export default App;
