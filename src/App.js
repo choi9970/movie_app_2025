a0944ed6d7bfcc008fe716af7fb530971f0b9fc3
choi9970 <j.choi9970@gmail.com>
2025-07-18 00:43:21 +0900

    동적인 요소 변화 state

--- src/App.js
+++ src/App.js
@@ -1,45 +1,31 @@
-import PropTypes, { string } from "prop-types";
+import React from "react";
 
-function Food({name,picture,rating}){
-  
-  return (
+class App extends React.Component{
+  state = {
+    count:0,
+  };
+
+  add=()=>{
+    this.setState(current => ({
+      count: current.count +1  
+    }));
+  };
+
+  minus=()=>{
+    this.setState(current => ({
+      count: current.count -1  
+    }));
+  };
+
+  render(){
+    return (
     <div>
-      <h1>I LOVE {name}</h1>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name}/>
+      <h1>난 테스트{this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
-    
-  );
-}
-
-const foodLike =[
-  {
-    id:1,
-    name:'kimchi',
-   image:'https://shop.hansalim.or.kr/shopping/is/itm/100101024/100101024_1_568.jpg',
-   rating : 5
+    );
   }
-  ,{
-    id:2,
-    name:'ramen',
-   image:'https://m.health.chosun.com/site/data/img_dir/2022/09/23/2022092301908_0.jpg',
-   rating : 4.9}
-];
-
-function App() {
-  //return <div className="App"/>;
-  return (
-    <div>
-        
-      {foodLike.map(dish=> (<Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>))}
-    </div>
-  );
 }
-
-Food.propTypes ={
-  name:PropTypes.string.isRequired,
-  image:PropTypes.string.isRequired,
-  rating:PropTypes.number
-};
 
 export default App;
